7e8fba3a6a857bf07408e6e3808873bfeb23c744
Wajidur Rahman <wrfuad0@gmail.com>
2021-08-11 18:42:59 +0600

    first commit

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDYS5XCoIPrmEoMN8yedmdXwpaNGaTkitA",
+  authDomain: "ema-jon-simple-c13ed.firebaseapp.com",
+  projectId: "ema-jon-simple-c13ed",
+  storageBucket: "ema-jon-simple-c13ed.appspot.com",
+  messagingSenderId: "589566830133",
+  appId: "1:589566830133:web:4cea5bda68587b3f3d337c"
+  };
+  export default firebaseConfig;
